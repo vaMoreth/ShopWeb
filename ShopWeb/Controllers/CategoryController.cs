110f31d910032d84c1583ce9afa2317bb5630f56
vaMoreth <jummyvasya24@gmail.com>
2024-01-11 09:53:12 +0100

    retriving data from db to controller

--- ShopWeb/Controllers/CategoryController.cs
+++ ShopWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopWeb.Data;
+using ShopWeb.Models;
 
 namespace ShopWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
