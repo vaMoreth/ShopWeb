3312ae2aa51ace8abe153c5a183c84f8a27ef8e4
vaMoreth <jummyvasya24@gmail.com>
2024-01-14 11:59:24 +0100

    added Edit and Delete btn and wrote find of id category

--- ShopWeb/Controllers/CategoryController.cs
+++ ShopWeb/Controllers/CategoryController.cs
@@ -17,6 +17,8 @@ namespace ShopWeb.Controllers
             return View(objCategoryList);
         }
 
+        //Create Page
+
         public IActionResult Create()
         {
             return View();
@@ -29,6 +31,38 @@ namespace ShopWeb.Controllers
                 ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
             }
             if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View(obj);
+        }
+
+        // Edit Page
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            }
+            Category? categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
