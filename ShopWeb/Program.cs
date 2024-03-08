4e464dcd5cbce72cd2a5d72e500584703f874cfe
vaMoreth <jummyvasya24@gmail.com>
2024-03-08 19:45:46 +0100

    replase DbContext to Category Repository

--- ShopWeb/Program.cs
+++ ShopWeb/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Shop.DataAccess.Repositories;
+using Shop.DataAccess.Repositories.IRepository;
 using Shop.DataAcess.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option => 
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
