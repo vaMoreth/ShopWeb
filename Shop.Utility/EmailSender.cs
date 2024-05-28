05384c0c1e6f657935b599e4e02de4e2ae4728e4
vaMoreth <jummyvasya24@gmail.com>
2024-05-28 18:15:50 +0200

    Add role for users and add fake email sender

---
+++ Shop.Utility/EmailSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Shop.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //in the future it will be logic to send email
+            return Task.CompletedTask;
+        }
+    }
+}
