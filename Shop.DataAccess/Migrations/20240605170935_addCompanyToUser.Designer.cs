189aca42d2d2d15877d142b5306a698e0c9cdfe8
vaMoreth <jummyvasya24@gmail.com>
2024-06-05 19:20:02 +0200

    Add company to user and add dropdown menu for selecting company

--- Shop.DataAccess/Migrations/20240605170935_addCompanyToUser.Designer.cs
+++ Shop.DataAccess/Migrations/20240605170935_addCompanyToUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shop.DataAcess.Data;
 
@@ -11,9 +12,11 @@ using Shop.DataAcess.Data;
 namespace Shop.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240605170935_addCompanyToUser")]
+    partial class addCompanyToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
