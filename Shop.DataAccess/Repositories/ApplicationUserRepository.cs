e83be58bc299d30f698982bdc48c2d3c6c6992f6
vaMoreth <jummyvasya24@gmail.com>
2024-06-06 08:55:57 +0200

    Add ApplicationUser Repositories

---
+++ Shop.DataAccess/Repositories/ApplicationUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using Shop.DataAccess.Repositories.IRepository;
+using Shop.DataAcess.Data;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repositories
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private ApplicationDbContext _db;
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
--- Shop.DataAccess/Repositories/UnitOfWork.cs
+++ Shop.DataAccess/Repositories/UnitOfWork.cs
@@ -16,6 +16,7 @@ namespace Shop.DataAccess.Repositories
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
         public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
@@ -23,6 +24,7 @@ namespace Shop.DataAccess.Repositories
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
         
 
