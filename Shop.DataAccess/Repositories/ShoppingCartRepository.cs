88b7a4e9a9fe84a5671d4128ec03afc30bf37782
vaMoreth <jummyvasya24@gmail.com>
2024-06-06 08:52:08 +0200

    Add shopping cart to repository

---
+++ Shop.DataAccess/Repositories/ShoppingCartRepository.cs
@@ -0,0 +1,26 @@
+﻿using Shop.DataAccess.Repositories.IRepository;
+using Shop.DataAcess.Data;
+using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DataAccess.Repositories
+{
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
