3895cef5adb2eb5b6b60b23055bf35cec6416403
vaMoreth <jummyvasya24@gmail.com>
2024-06-06 12:48:10 +0200

    Create Summary Page add UI for it

--- ShopWeb/Areas/Customer/Controllers/CartController.cs
+++ ShopWeb/Areas/Customer/Controllers/CartController.cs
@@ -40,6 +40,12 @@ namespace ShopWeb.Areas.Customer.Controllers
         }
 
         #region ActionMethods
+
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
         public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
