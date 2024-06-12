c29ac1075a7fccc3adb32c659f02bdb77e1661f5
vaMoreth <jummyvasya24@gmail.com>
2024-06-12 08:35:11 +0200

    Add two helper methods in OrderHeader repo

--- Shop.DataAccess/Repositories/IRepository/IOrderHeaderRepository.cs
+++ Shop.DataAccess/Repositories/IRepository/IOrderHeaderRepository.cs
@@ -10,5 +10,7 @@ namespace Shop.DataAccess.Repositories.IRepository
     public interface IOrderHeaderRepository : IRepository<OrderHeader>
     {
         void Update(OrderHeader obj);
+        void UpdateStatus(int id, string orderStatus, string? paymentStatus = null);
+        void UpdateStripPaymentID(int id, string sessionId, string paymentIntentId);
     }
 }
