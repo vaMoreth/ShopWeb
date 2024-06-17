8f215c425445bd4f757d82921e7b81b7f7347c10
vaMoreth <jummyvasya24@gmail.com>
2024-06-17 16:54:08 +0200

    Create OrderVM and OrderController

---
+++ ShopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Shop.DataAccess.Repositories.IRepository;
+using Shop.Models;
+
+namespace ShopWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region GetAll
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+        #endregion
+    }
+}
