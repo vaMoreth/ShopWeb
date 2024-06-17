19bdd8fe98ff1dbdfa407cb72e44342a75fb9f50
vaMoreth <jummyvasya24@gmail.com>
2024-06-17 18:12:45 +0200

    Add filters in Order List

--- ShopWeb/Areas/Admin/Controllers/OrderController.cs
+++ ShopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Shop.DataAccess.Repositories.IRepository;
 using Shop.Models;
+using Shop.Utility;
+using System.Diagnostics;
 
 namespace ShopWeb.Areas.Admin.Controllers
 {
@@ -23,9 +25,29 @@ namespace ShopWeb.Areas.Admin.Controllers
 
         #region GetAll
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
-            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            switch (status)
+            {
+                case "pending":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
+
             return Json(new { data = objOrderHeaders });
         }
 
