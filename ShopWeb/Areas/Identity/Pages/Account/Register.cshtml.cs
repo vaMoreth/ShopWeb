daa0d62eab1015d37ef352ecc27b7d547e718845
vaMoreth <jummyvasya24@gmail.com>
2024-07-02 18:10:16 +0200

    Implement DbInitializer

--- ShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ShopWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -128,13 +128,6 @@ namespace ShopWeb.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if(!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-            }
 
             Input = new()
             {
